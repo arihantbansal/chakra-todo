975991dd06303d0f9affb0b627933b5ea6d1cb97
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2021-07-22 21:54:38 +0530

    Added basic components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Heading, VStack, IconButton } from "@chakra-ui/react";
+import TodoList from "./components/TodoList";
+import AddTodo from "./components/AddTodo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<VStack>
+			<Heading>Todo Application</Heading>
+		</VStack>
+	);
+};
 
 export default App;
---
+++ src/components/AddTodo.js
@@ -0,0 +1,5 @@
+const AddTodo = () => {
+	return <div></div>;
+};
+
+export default AddTodo;
