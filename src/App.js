14ad7701a1147c8281a5b8fe380bfd8afc573916
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2021-07-22 23:34:54 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import { Heading, VStack, IconButton, useColorMode } from "@chakra-ui/react";
+import {
+	Heading,
+	VStack,
+	IconButton,
+	ButtonGroup,
+	useColorMode,
+	Box,
+	Stack,
+	Text,
+	Link,
+} from "@chakra-ui/react";
 import TodoList from "./components/TodoList";
 import AddTodo from "./components/AddTodo";
-import { FaSun, FaMoon } from "react-icons/fa";
+import { FaSun, FaMoon, FaLinkedin, FaTwitter, FaGithub } from "react-icons/fa";
 import { useState, useEffect } from "react";
 
 const App = () => {
@@ -55,6 +65,46 @@ const App = () => {
 			</Heading>
 			<TodoList todos={todos} deleteTodo={deleteTodo} />
 			<AddTodo addTodo={addTodo} />
+			<Box
+				as="footer"
+				role="contentinfo"
+				mx="auto"
+				maxW="7xl"
+				py="12"
+				px={{ base: "4", md: "8" }}>
+				<Stack>
+					<Stack
+						direction="row"
+						spacing="4"
+						align="center"
+						justify="space-between">
+						<ButtonGroup variant="ghost" color="gray.600">
+							<IconButton
+								as="a"
+								href="https://www.linkedin.com/in/arihantbansal/"
+								aria-label="LinkedIn"
+								icon={<FaLinkedin fontSize="20px" />}
+							/>
+							<IconButton
+								as="a"
+								href="https://github.com/arihantbansal"
+								aria-label="GitHub"
+								icon={<FaGithub fontSize="20px" />}
+							/>
+							<IconButton
+								as="a"
+								href="https://twitter.com/_arihantbansal"
+								aria-label="Twitter"
+								icon={<FaTwitter fontSize="20px" />}
+							/>
+						</ButtonGroup>
+					</Stack>
+					<Text fontSize="md" alignSelf={{ base: "center", sm: "start" }}>
+						&copy; {new Date().getFullYear()}{" "}
+						<Link href="http://arihantbansal.github.io/">Arihant Bansal</Link>
+					</Text>
+				</Stack>
+			</Box>
 		</VStack>
 	);
 };
